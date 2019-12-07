0406e47d6dc4cb970160eaa32e4d27eff24d8042
joelcarlss <jc222mw@student.lnu.se>
2019-12-07 09:47:15 +0100

    continuing build of page_rank

--- server/page_rank.py
+++ server/page_rank.py
@@ -9,19 +9,30 @@ def get_data():
 		path = str.split(file_name, '/')
 		file = open(file_name).read()
 		file = file.replace('/wiki/', '')
-		array = pd.Index(file.split('\n')[:-1])
-		df = df.append({'name': path[4], 'links': array}, ignore_index=True)
+		array = file.split('\n')[:-1]
+		df = df.append({'name': path[4], 'links': array, 'page_rank': 1}, ignore_index=True)
 	# length links arr at index 0
 	# print(len(df.iloc[0]['links']))
 	return df
 
+
 def calculate_page_rank():
 	test = 'Computer_scientist'
 	df = get_data()
-	print(df['name'])
 	# https://stackoverflow.com/questions/53342715/pandas-dataframe-select-rows-where-a-list-column-contains-any-of-a-list-of-strin
-	print(df[pd.DataFrame(df['links'].tolist()).isin([test]).any(1)]) # This hopefully writes out all docs that contains test
+	# print(df[pd.DataFrame(df['links'].tolist()).isin([test]).any(1)]) # Writes out all docs that contains test
+	for index, row in df.iterrows(): # index makes the row readable in right direction. how?!
+		links = df[pd.DataFrame(df['links'].tolist()).isin([row['name']]).any(1)]  # Writes out all docs that contains test
+		#  print(len(links))
+		#  print()
+		iterate_page_rank(links)
 
+		break
+
+
+def iterate_page_rank(linked_docs):
+	for index, link_row in linked_docs.iterrows():
+		print(link_row)
 
 
 calculate_page_rank()
